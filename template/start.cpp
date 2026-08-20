1e9f78efb4ad22e2fe409640499f3e3e8106c4b0
thangdz2k7 <128198843+thangdz2k7@users.noreply.github.com>
2026-08-20 20:44:43 +0800

    Create  start.cpp

---
+++ template/start.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define fi first
+#define se second
+#define endl "\n"
+
+using namespace std;
+using ll = long long;
+using ii = pair <int, int>;
+using vi = vector <int>;
+
+void maxl(auto &a, auto b) {a = max(a, b);}
+void minl(auto &a, auto b) {a = min(a, b);}
+
+const int N = 1e5 + 5;
+const int mod = 1e9 + 7;
+
+void process(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    //int t; cin >> t; while (t --)
+    process();
+
+    return 0;
+}
