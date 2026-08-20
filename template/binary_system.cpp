3f7254ff164f528deb6954a3ff0888f30dd6770c
thangdz2k7 <128198843+thangdz2k7@users.noreply.github.com>
2026-08-20 20:46:13 +0800

    Create binary_system.cpp

---
+++ template/binary_system.cpp
@@ -0,0 +1,8 @@
+// support binary system
+
+#define Mask(i)  (1 << (i))
+#define Bit(x, i)  ((x) >> (i) & 1)
+#define bp __builtin_popcount
+#define bpll __builtin_popcountll
+#define c0 __builtin_clz
+#define c0ll __builtin_clzll
