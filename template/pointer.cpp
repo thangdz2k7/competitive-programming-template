91b9b0f004d38ae287028a76277097b7fd3f7a8a
thangdz2k7 <128198843+thangdz2k7@users.noreply.github.com>
2026-08-20 21:30:45 +0800

    Create pointer.cpp

---
+++ template/pointer.cpp
@@ -0,0 +1,9 @@
+// pointer
+
+static char buf[200 << 20];
+void* operator new(size_t s) {
+    static size_t i = sizeof buf;
+    assert(s < i);
+    return (void*) &buf[i -= s];
+}
+void operator delete(void*) {}
