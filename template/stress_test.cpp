6764c7d6cc72b48fb7bf05d76bb9b8ec5820d6a8
thangdz2k7 <128198843+thangdz2k7@users.noreply.github.com>
2026-08-20 20:48:02 +0800

    Create stress_test.cpp

---
+++ template/stress_test.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Tên chương trình
+const string NAME = "wpumps";
+// Số test kiểm tra
+const int NTEST = 10000;
+
+
+// Viết lại hàm random để sử dụng cho thuận tiện. Hàm random này sinh ngẫu nhiên số trong phạm vi long long, số sinh ra >= l và <= h.
+long long Rand(long long l, long long h) {
+    return 1ll * rand() * rand() % (h - l + 1) + l;
+}
+
+int main()
+{
+    srand(time(NULL));
+    for (int iTest = 1; iTest <= NTEST; iTest++)
+    {
+        ofstream inp((NAME + ".inp").c_str());
+
+        inp.close();
+        // Nếu dùng Linux thì "./" + Tên chương trình
+        system((NAME + ".exe").c_str());
+        system((NAME + "_trau.exe").c_str());
+        // Nếu dùng linux thì thay fc bằng diff
+        if (system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0)
+        {
+            cout << "Test " << iTest << ": WRONG!\n";
+            return 0;
+        }
+        cout << "Test " << iTest << ": CORRECT!\n";
+    }
+    return 0;
+}
